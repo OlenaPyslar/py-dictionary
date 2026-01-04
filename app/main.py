ce3265c9fc460d1ca10aa7bc129bc09c23d6444b
Olena Pyslar <pislar33@gmail.com>
2026-01-04 17:51:11 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,55 @@
+from typing import Any
+
+
 class Dictionary:
-    pass
+    def __init__(self, initial_capacity=8, load_factor=0.75) -> None:
+        self.capacity = initial_capacity
+        self.load_factor = load_factor
+        self.table = []
+        for _ in range(initial_capacity):
+            self.table.append([])
+        self.size = 0
+
+    def __setitem__(self, key: int, value: Any) -> None:
+        hash_key = hash(key)
+        index = hash_key % self.capacity
+
+        bucked = self.table[index]
+
+        for node in bucked:
+            if node[0] == key:
+                node[1] = value
+                return
+
+        bucked.append([key, value])
+        self.size += 1
+
+        if self.size / self.capacity >= self.load_factor:
+            self._resize()
+
+    def _resize(self) -> None:
+        new_capacity = self.capacity * 2
+        new_table = [[] for _ in range(new_capacity)]
+        for bucked in self.table:
+            for node in bucked:
+                key, value = node
+                new_index = hash(key) % new_capacity
+                new_table[new_index].append([key, value])
+
+        self.table = new_table
+        self.capacity = new_capacity
+
+    def __getitem__(self, key: int) -> Any:
+        hash_key = hash(key)
+        index = hash_key % self.capacity
+        bucked = self.table[index]
+
+        for node in bucked:
+            if node[0] == key:
+                return node[1]
+        raise KeyError
+
+    def __len__(self) -> int:
+        return self.size
+
+
